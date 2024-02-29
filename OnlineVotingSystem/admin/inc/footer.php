6510ac2e02997fae89f99cbe5973bf1b103c7292
Gaurav Singhal <161643410+Gaurav8860@users.noreply.github.com>
2024-02-29 23:02:35 +0530

    Add files via upload

---
+++ OnlineVotingSystem/admin/inc/footer.php
@@ -0,0 +1,16 @@
+
+    <div class="row bg-black text-center text-white">
+        <div class="col-12 my-3">
+            <p> &3
+                ; PROJECT 2023  <br />
+                Developed by MANSI & GAURAV 
+            </p>
+        </div>
+    </div>
+
+</div>
+
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+</body>
+</html>
